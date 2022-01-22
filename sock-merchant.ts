8a7f36c1edba86ec9e0ced86cd9ec3e0c5d96068
amildie <amildie@gmail.com>
2022-01-22 20:13:35 -0300

    sock-merchant

---
+++ sock-merchant.ts
@@ -0,0 +1,66 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n: number, ar: number[]): number {
+  let res = 0;
+  let pairs: any = {};
+
+  for(let i = 0; i < ar.length; ++i) {
+    if(!pairs[ar[i]]) {
+      pairs[ar[i]] = 1;
+    } else {
+      pairs[ar[i]]++;
+    }
+  }
+
+  for (let key in pairs) {
+    res += Math.floor(pairs[key]/2);
+  }
+  
+  return res;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const ar: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result: number = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
